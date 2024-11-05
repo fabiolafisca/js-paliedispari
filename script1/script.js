989bdfd615d9e3fbfc61ca7d92300c75ca03fae8
fabiolafisca <fabiolafisca@hotmail.com>
2024-11-05 17:51:41 +0100

    esercizio palidroma e pari e dispari, javascript

--- script1/script.js
+++ script1/script.js
@@ -4,8 +4,6 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
-
-
 let parola = prompt(
 "inserire una parola per verificare se è palindroma"
 ).toLowerCase;
